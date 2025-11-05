3b83da59785257ddae357dfb23371e9da46ea961
REYES Alexi <ar222896@dwnnfc9c6w.edu.atlas.local>
2025-11-05 09:29:05 +0100

    first commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by REYES Alexi on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
